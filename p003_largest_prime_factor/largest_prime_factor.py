e5f45bf731aae519809b9675cfeed9d357ec60b5
Joyce Chen <yuhsinc@gmail.com>
2017-12-30 19:28:04 -0800

    try to optimize and profile

--- p003_largest_prime_factor/largest_prime_factor.py
+++ p003_largest_prime_factor/largest_prime_factor.py
@@ -6,7 +6,7 @@
 
 # Implement prime sieve.
 
-import math
+import time
 
 def primes(max_num):
   prime_bitmap = [True for x in xrange(max_num)]
@@ -32,5 +32,34 @@ def prime_factors(num):
     num /= prime_factor
     yield prime_factor
 
-max(prime_factors(600851475143))
+t0 = time.time()
+result = max(prime_factors(600851475143))
+t1 = time.time()
+print('prime_factors(600851475143) time: ' + str(t1 - t0))
+# result: 6857
 
+# If we create the bitmap only once, is it more efficient?
+def largest_prime_factor(num):
+  largest = 1
+  max_factor = int(num ** 0.5) + 1
+  prime_bitmap = [True for x in xrange(max_factor)]
+  prime_bitmap[0] = False
+  prime_bitmap[1] = False
+  for i in xrange(max_factor):
+    if prime_bitmap[i]:
+      # i is prime.
+      for j in xrange(i**2, max_factor, i):
+        prime_bitmap[j] = False
+      while num % i == 0:
+        # i is a prime factor of num.
+        largest = i
+        num /= i
+        if num == 1:
+          return largest
+  return num
+
+t0 = time.time()
+result = largest_prime_factor(600851475143)
+t1 = time.time()
+print('largest_prime_factor(600851475143) time: ' + str(t1 - t0))
+# result: 6857
