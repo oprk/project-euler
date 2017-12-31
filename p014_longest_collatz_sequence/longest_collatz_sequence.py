f56456dea797573c1b2e3ccf2fb7d1e1fe37e597
Joyce Chen <yuhsinc@gmail.com>
2017-12-30 22:06:10 -0800

    p014 comment nits

--- p014_longest_collatz_sequence/longest_collatz_sequence.py
+++ p014_longest_collatz_sequence/longest_collatz_sequence.py
@@ -8,7 +8,8 @@
 #   13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
 #
 # It can be seen that this sequence (starting at 13 and finishing at 1) contains
-# 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+# 10 terms. Although it has not been proved yet (Collatz Problem), it is thought
+# that all starting numbers finish at 1.
 #
 # Which starting number, under one million, produces the longest chain?
 #
@@ -27,6 +28,7 @@ def collatz_length(num):
   return memo[num]
 
 t0 = time.time()
+
 best_length = 1
 best_number = 1
 for i in xrange(1, 10**6):
@@ -34,6 +36,7 @@ for i in xrange(1, 10**6):
   if best_length < length:
     best_length = length
     best_number = i
+
 t1 = time.time()
   
 print(best_number)
